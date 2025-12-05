06d1208d1a69b089f7d20a5f027f11dea13d0004
Hugo Rzepian Teixeira <hugorteixeira@senhormercado.com.br>
2025-12-05 11:36:10 -0300

    up

--- R/html_parse.R
+++ R/html_parse.R
@@ -550,7 +550,7 @@
       function(token) grepl(token, sanitized[i], fixed = TRUE),
       logical(1)
     )]
-    needs_expansion <- !successfully_standardized[i] || length(hits) > 1L
+    needs_expansion <- !successfully_standardized[i]
     if (needs_expansion && length(hits)) hits else character()
   })
   dup <- duplicated(canonical)
